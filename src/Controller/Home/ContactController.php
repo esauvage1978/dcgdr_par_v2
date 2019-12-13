3a4e74d03caa9365894bd056dbb19feb390c0628
SAUVAGE <emmanuel.sauvage@live>
2019-12-13 22:01:33 +0100

    #01 create security

--- src/Controller/Home/ContactController.php
+++ src/Controller/Home/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Home;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +15,10 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact", methods={"GET"})
      * @return Response
+     * @IsGranted("ROLE_USER")
      */
     public function contactAction(): Response
     {
-
         return $this->render('home/contact.html.twig', []);
     }
-
-
 }
